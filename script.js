f1f7cb7229957212c77afe0d693fe8325802fc5c
Joel D'Autilio <joeldautilio@gmail.com>
2023-01-02 10:59:45 -0300

    Complete trending section

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const projectsDiv = document.querySelector('.projects');
 const announcementsDiv = document.querySelector('.announcements');
+const trendingDiv = document.querySelector('.trending');
 
 const githubButton = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>';
 const starButton = '<svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M5.8 21L7.4 14L2 9.2L9.2 8.6L12 2L14.8 8.6L22 9.2L18.8 12H18C17.3 12 16.6 12.1 15.9 12.4L18.1 10.5L13.7 10.1L12 6.1L10.3 10.1L5.9 10.5L9.2 13.4L8.2 17.7L12 15.4L12.5 15.7C12.3 16.2 12.1 16.8 12.1 17.3L5.8 21M17 14V17H14V19H17V22H19V19H22V17H19V14H17Z" /></svg>';
@@ -21,8 +22,16 @@ const announcements = [
   {title: "Update privacy policy", content: "Aliquam venenatis, libero consectetur iaculis eleifend, lectus tellus euismod nibh, eget volutpat nunc diam in neque. Integer eleifend ex quis fringilla dictum. Vivamus mi est, facilisis sit amet venenatis eget, porta a ex."},
 ];
 
+const trendingUsers = [
+  {username: "tegan", text: "World Peace Builder", backgroundColor: "aliceblue"},
+  {username: "morgan", text: "Super Cool Project", backgroundColor: "#FFCA28"},
+  {username: "kendall", text: "Life Changing App", backgroundColor: "#7C4DFF"},
+  {username: "alex", text: "No Traffic Maker", backgroundColor: "#F48FB1"},
+];
+
 projects.forEach(createCard);
 announcements.forEach(createAnnouncement);
+trendingUsers.forEach(createTrending);
 
 function createCard(project) {
   /*
@@ -77,4 +86,32 @@ function createAnnouncement(announcement) {
   let content = document.createElement('p');
   content.textContent = announcement.content;
   ann.appendChild(content);
+}
+
+function createTrending(trendingUser) {
+  /*
+   *  <div class="profile-pic"></div>
+   *  <div>
+   *    <span>@username</span><br>
+   *    <span>text</span>
+   *  </div>
+   */
+  const profilePic = document.createElement('div');
+  trendingDiv.appendChild(profilePic);
+  profilePic.classList.add('profile-pic');
+  profilePic.style.backgroundColor = trendingUser.backgroundColor;
+
+  const rightText = document.createElement('div');
+  trendingDiv.appendChild(rightText);
+
+  const username = document.createElement('span');
+  rightText.appendChild(username);
+  username.textContent = `@${trendingUser.username}`;
+
+  rightText.appendChild(document.createElement('br'));
+
+  const text = document.createElement('span');
+  rightText.appendChild(text);
+  text.textContent = trendingUser.text;
+  text.style.color = "var(--grey-foreground, grey)";
 }
